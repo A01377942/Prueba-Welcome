c6bec3759401fb14af24826fb29a1495ff706bc4
A01377942 <A01377942@itesm.mx>
2023-05-11 11:18:21 -0600

    Metadata Update

--- app/page.js
+++ app/page.js
@@ -4,13 +4,6 @@ import Main from '../components/main'
 import RootLayout from './layout'
 import styles from '../styles/page.module.css'
 
-export const metadata = {
-  title: 'Explore the world as an International Travel Agent',
-  description: 'Become a travel agent and get great prices with Volindo. Discover a new way of traveling and save money on your next adventures.',
-  keywords: 'travel, internacional travel, airplane'
-}
-
-
 export default function Home() {
 
   return (
